ceec1ea64bf326c0b2412e60e31060c6f1099802
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-09-13 16:19:31 -0400

    Get Products Paged

--- src/main/java/com/tads4/tads4/services/ProductService.java
+++ src/main/java/com/tads4/tads4/services/ProductService.java
@@ -4,9 +4,12 @@ import com.tads4.tads4.dto.ProductDTO;
 import com.tads4.tads4.entities.Product;
 import com.tads4.tads4.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +22,13 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return  result.map(x -> new ProductDTO(x));
 
     }
 }
