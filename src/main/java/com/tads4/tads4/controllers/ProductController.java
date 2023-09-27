ad0515a0b2ac3b347de3473ea559db5fb4e78a03
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-09-27 11:05:17 -0400

    PUT Product

--- src/main/java/com/tads4/tads4/controllers/ProductController.java
+++ src/main/java/com/tads4/tads4/controllers/ProductController.java
@@ -46,4 +46,11 @@ public class ProductController {
        return ResponseEntity.created(uri).body(dto);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> updated(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+
+    }
+
 }
