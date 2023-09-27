b7473104ff633207435a8519eeed64498858d85a
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2023-09-27 10:07:29 -0400

    Método POST

--- src/main/java/com/tads4/tads4/controllers/ProductController.java
+++ src/main/java/com/tads4/tads4/controllers/ProductController.java
@@ -1,15 +1,17 @@
 package com.tads4.tads4.controllers;
 
+
 import com.tads4.tads4.dto.ProductDTO;
+import com.tads4.tads4.entities.Product;
+import com.tads4.tads4.repositories.ProductRepository;
 import com.tads4.tads4.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -19,27 +21,22 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
-        ProductDTO dto = service.findById(id);
-        return ResponseEntity.ok(dto);
+    public ProductDTO findById(@PathVariable Long id) {
+        return  service.findById(id);
 
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(pageable);
-        return ResponseEntity.ok(dto);
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return  service.findAll(pageable);
 
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
-       dto = service.insert(dto);
-       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
-               buildAndExpand(dto.getId()).toUri();
-       return ResponseEntity.created(uri).body(dto);
-
-
+    public ProductDTO inserir (@RequestBody ProductDTO dto) {
+        /*dto = service.insert(dto)
+        return  dto;*/
+        return service.insert(dto);
     }
 
 }
